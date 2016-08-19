afc8241ee26ce35ec3e2eebcaf4592a8bacfcfc8
weiyang wan <weiyangwan@gmail.com>
2016-08-19 09:46:51 +0800

    Refractor the charts

--- scripts/chart.js
+++ scripts/chart.js
@@ -21,56 +21,46 @@ var axis = d3.svg.axis().scale(widthScale);
 
 //Creating the canvas
 // https://github.com/d3/d3/blob/master/API.md#selections-d3-selection
-var Chart = d3.select("#barChart").
+var chartCanvas = d3.select("#barChart").
   append("svg").
   attr("width", canvasWidth + barMarginLeft + 30).
   attr("height", barHeight * ( data.length + 2 ));
 
 //Creating the bars
-Chart.selectAll("rect").
+chartCanvas.selectAll("rect").
   data(data).
   enter().
   append("rect").
   attr("x", barMarginLeft).
+  attr("y", function(d, i)  { return i * barHeight; }).
   attr("width", function(d) { return widthScale(d.rating); }).
   attr("height", barHeight - 3).
   attr("rx", 5).
-  attr("fill", "#005aff").
-  attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+  attr("fill", "#005aff");
 
-Chart.selectAll("text").
+chartCanvas.selectAll("text").
   data(data).
   enter().
   append("text").
   attr("x", barMarginLeft + 10).
-  attr("y", barHeight/2 - 1).
+  attr("y", function(d, i)  { return (i * barHeight) + barHeight/2; }).
   attr("dy", ".35em").
   text(function(d) {return d.rating; }).
   attr("style", "font-size: 0.8em; font-family: Helvetica, sans-serif").
-  attr("fill", "white").
-  attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+  attr("fill", "white");
 
-Chart.selectAll("text.yAxis").
+chartCanvas.selectAll("text.yAxis").
   data(data).
   enter().
   append("text").
   attr("x", barMarginLeft - 10).
-  attr("y", barHeight/2 + 3).
+  attr("y", function(d, i)  { return (i * barHeight) + barHeight/1.5; }).
   text(function(d) { return d.brand; } ).
   attr("style", "font-size: 0.8em; font-family: Helvetica, sans-serif").
-  attr("text-anchor", "end").
-  attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+  attr("text-anchor", "end");
 
-Chart.append("g").
+chartCanvas.append("g").
   attr("transform", "translate(" + barMarginLeft + "," + (barHeight * data.length) + ")").
   attr("style", "font-size: 0.8em; font-family: Helvetica, sans-serif").
   attr("fill", "#89b3ff").
   call(axis);
-
-  // var y = d3.scale.linear().domain([0, d3.max(data, function(d) { return d.rating; })]).rangeRound([0, barHeight]);
-
-//
-// hChart.selectAll("text.xAxis").
-//   data(data).
-//   enter().
-//   append("text").
