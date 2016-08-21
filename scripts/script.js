5cddbe7842b61dcc1e4ea5ab9d0e82823a8f9880
meeniebeanie <minyen.j@gmail.com>
2016-08-21 16:45:45 +0800

    SCROLLING ANIMATION WORKZ

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,11 @@ $(function() {
 
   $("#brandname").fadeIn(4000);
 
+  $("#arrow").click(function() {
+    console.log("clicked");
+    $('html,body').animate({
+        scrollTop: $("#title").offset().top},
+        'slow');
+});
+
 });
